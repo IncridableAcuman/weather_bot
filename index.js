34d6814529e7bf3cf2ede1388f6d03578599cf39
Izzatbek <abdusharipovizzat03@gmail.com>
2025-10-21 13:47:06 +0500

    weather bot

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require("express");
 const axios = require("axios");
 const redis = require("redis");
-const {Telegraf} = require("telegraf");
+const {Telegraf,Markup} = require("telegraf");
 require("dotenv").config();
 
 const telebotToken = process.env.TELEBOT_TOKEN || null;
 
-const app = express();
 const bot = new Telegraf(telebotToken);
 
-bot.start((msg)=>msg.reply("Hi"));
+bot.help((msg)=>msg.reply("Yordam xizmati hozircha ishlamaydi."));
+
+bot.command("start",(ctx)=>{
+    return ctx.reply(
+        "Assalomu aleykum",
+        Markup.keyboard([
+            ["Uzbekistan","Tashkent"]
+        ])
+        .oneTime()
+        .resize()
+    );
+});
 bot.launch();
 
 process.once("SIGINT",()=>bot.stop());
 process.once("SIGTERM",()=>bot.stop());
+console.log("Telegram bot running...");
